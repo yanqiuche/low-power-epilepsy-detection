21f243a986de9d40fda62e5206469056a7f19251
Joey van Rijn <jmsvanrijn@github.com>
2020-06-09 15:04:44 +0200

    Two seperate loaders for different cases

--- src/models/epilepsy_data_loader.py
+++ src/models/epilepsy_data_loader.py
@@ -4,7 +4,7 @@ import random
 import numpy as np
 
 
-class EpilepsyData(Dataset):
+class OnePerFileEpilepsyData(Dataset):
     def __init__(self, data, window_size):
         # Get directory listing from path
         self.window_size = window_size
@@ -17,11 +17,25 @@ class EpilepsyData(Dataset):
 
         loc = random.randint(0, len(signals[0]) - self.window_size)
         signals_cut = signals[:, loc: loc + self.window_size]
-        signal_mean = []
-        # for i in range(int(len(signals_cut[0])/2)-1):
-        #     t = 2*i
-        #     signal_mean.append(signals_cut[:, t] + signals_cut[:, t+1])
         return signals_cut, int(label), filename
+
+    def __len__(self):
+        return self.length
+
+
+class MultiplePerFileEpilepsyData(Dataset):
+    def __init__(self, data, window_size):
+        # Get directory listing from path
+        self.window_size = window_size
+        self.items = data
+        self.length = len(self.items)
+
+    def __getitem__(self, index):
+        filename, label, start = self.items[index]
+        signals, signal_headers, header = highlevel.read_edf(filename)
+
+        signal_cut = signals[:, int(start)*self.window_size: (int(start)+1)*self.window_size]
+        return signal_cut, int(label)
 
     def __len__(self):
         return self.length
